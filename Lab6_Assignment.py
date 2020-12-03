ce209400eafb6ccbadbd81f72e3aaf6d42fcb5b6
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-03 13:44:35 -0800

    plain english complete

---
+++ Lab6_Assignment.py
@@ -0,0 +1,14 @@
+
+"""
+To start, we will generate a random interger between interger between 1 and 20, and
+based on the result of the random numbers, we check to see if it falls under certain range
+if the number is > than 15, then the results will be "Cherries"
+otherwise if the number is > 10, then the results will be "Orange"
+otherwise if the number is > 5, then the results will be "Plum"
+otherwise if the number is > 2, then the results will be "Melon"
+otherwise, if the number is > 1, then the results will be "Bell"
+If the number is not any of the above,  then the results will be "Bar"
+
+we iterate over using a loop three times and print the results to the user. As an example "Plum Cherries"
+"""
+
